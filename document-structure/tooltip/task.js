1d9d3f74cbd74c2fc53e21d558fdb96e88db8662
Dmitriy Kamay <kamay.dmitriy@gmail.com>
2025-01-26 17:55:22 +0300

    second commit tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,10 +1,20 @@
+const elemWithTT = Array.from(document.querySelectorAll('.has-tooltip'));
 let selectedTooltip = null;
+
+elemWithTT.forEach((element, index, array) => {
+    element.dataset.id = array.indexOf(element);
+})
 
 document.body.addEventListener('click', (e) => {
     e.preventDefault();
     if (e.target.classList.contains('has-tooltip')) {
+        if (e.target.dataset.id === selectedTooltip?.dataset.id) {
+            console.log(e.target);
+            e.target.nextElementSibling.classList.toggle('tooltip_active');
+        } else {
         insertTooltip(e.target);
         showTooltip(e.target);
+        }
     }
 })
 
@@ -16,11 +26,11 @@ function insertTooltip(item) {
 
 function showTooltip(item) {
     if (selectedTooltip) {
-        selectedTooltip.classList.remove('tooltip_active');
+        selectedTooltip.nextElementSibling.classList.remove('tooltip_active');
     }
     const getTooltip = item.nextElementSibling;
     getTooltip.classList.add('tooltip_active');
-    selectedTooltip = getTooltip;
+    selectedTooltip = item;
     getTooltip.style.position = 'absolute';
     const currentPos = getTooltip.getBoundingClientRect();
     const targetPos = item.getBoundingClientRect();
