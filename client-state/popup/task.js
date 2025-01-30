1216866e3b676d4c400f9d336f8179f52885c860
Dmitriy Kamay <kamay.dmitriy@gmail.com>
2025-01-30 16:48:02 +0300

    first commit popup

---
+++ client-state/popup/task.js
@@ -0,0 +1,48 @@
+const subscribeModal = document.getElementById('subscribe-modal');
+const modalClose = document.querySelector('.modal__close');
+
+checkModalWindowStatus()
+
+function closeModalWindow() {
+    subscribeModal.classList.remove('modal_active');
+    setCookie('isCloseModalWindow', true);
+}
+
+function checkModalWindowStatus() {
+    if (!getCookie('isCloseModalWindow')) {
+        subscribeModal.classList.add('modal_active');
+        modalClose.addEventListener('click', closeModalWindow);
+    }  
+}
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
+
+function setCookie(name, value, options = {}) {
+
+  options = {
+    path: '/',
+    // при необходимости добавьте другие значения по умолчанию
+    ...options
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
